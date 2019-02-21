43c918d514fab45e1770a57ecdf5553a13c55d0d
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-02-21 20:48:22 -0300

    Entendendo o ciclo de vida de uma session bean

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -2,16 +2,27 @@ package br.com.caelum.livraria.dao;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import br.com.caelum.livraria.modelo.Autor;
 
-//Transforma em EJB
+//Transforma em para ser gerenciado pelo container EJB
+//Thread safe, o proprio ejb cria outra instancia caso esta esteja ocupada.
 @Stateless
 public class AutorDao {
 
-	private Banco banco = new Banco();
+	@Inject
+	private Banco banco;
 
+	//É chamado após a criação da Injeção de dependência, Callback
+	@PostConstruct
+	void aposCriacao() {
+		System.out.println(getClass().getSimpleName()+" foi criado com sucesso");
+	}
+	
+	
 	public void salva(Autor autor) {
 		banco.save(autor);
 	}
--- src/br/com/caelum/livraria/dao/Banco.java
+++ src/br/com/caelum/livraria/dao/Banco.java
@@ -3,10 +3,16 @@ package br.com.caelum.livraria.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
 import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
 import br.com.caelum.livraria.modelo.Usuario;
 
+@Singleton//Limita a um objeto
+@Startup//Inicializado logo no inicio do carregamento da aplicação
 public class Banco {
 	
 	public static List<Livro> livros = new ArrayList<Livro>();
@@ -17,6 +23,11 @@ public class Banco {
 	
 	static {
 		inicializaBanco();
+	}
+	
+	@PostConstruct
+	void aposCriacao() {
+		System.out.println(getClass().getSimpleName()+" foi criado com sucesso");
 	}
 
 	public void save(Livro livro) {
